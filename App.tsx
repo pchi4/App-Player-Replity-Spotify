d7a919f27c98b608d86dfadea36b9a468562a6e7
Douglas Lopes <pchialoopes@gmail.com>
2024-02-08 10:14:24 -0300

    FEATURE: Remove hook

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ import { StateProvider } from "./src/context/State";
 import { Controller } from "./src/components/Controller";
 import { useStateValue } from "./src/context/State";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useVerifyToken, useVerifyAlbum, useRefreshToken } from "./src/hooks/";
+import { useVerifyToken, useVerifyAlbum } from "./src/hooks/";
 
 export default function App() {
   const queryClient = new QueryClient();

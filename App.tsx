e155bb5bc5248458868672055fea255819f0df2f
Douglas Lopes <pchialoopes@gmail.com>
2024-05-15 10:00:15 -0300

    FEATURE: Eject Expo

--- App.tsx
+++ App.tsx
@@ -5,36 +5,15 @@ import { StatusBar } from "native-base";
 import Routes from "./src/routes";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { StateProvider } from "./src/context/State";
-import TrackPlayer, { Capability } from "react-native-track-player";
 
 export default function App() {
   const queryClient = new QueryClient();
-  // AppRegistry.registerComponent(...);
-
-  async function setup() {
-    await TrackPlayer.setupPlayer({});
-    await TrackPlayer.updateOptions({
-      capabilities: [
-        Capability.Play,
-        Capability.Pause,
-        Capability.SkipToNext,
-        Capability.SkipToPrevious,
-        Capability.Stop,
-        Capability.SeekTo,
-      ],
-      compactCapabilities: [Capability.Play, Capability.Pause],
-    });
-  }
-
-  React.useEffect(() => {
-    setup();
-  }, []);
 
   return (
     <StateProvider>
       <QueryClientProvider client={queryClient}>
         <NativeBaseProvider>
-          <StatusBar />
+          <StatusBar barStyle="light-content" />
           <Routes />
         </NativeBaseProvider>
       </QueryClientProvider>
--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-import { AppRegistry } from "react-native";
-import { registerRootComponent } from "expo";
-import TrackPlayer from "react-native-track-player";
-import { PlaybackService } from "./src/services/PlaybackService";
+import { registerRootComponent } from 'expo';
 
-import App from "./App";
-// AppRegistry.registerComponent("example", () => App);
+import App from './App';
+
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriately
 registerRootComponent(App);
-TrackPlayer.registerPlaybackService(() => PlaybackService);
